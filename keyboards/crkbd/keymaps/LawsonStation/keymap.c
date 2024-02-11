9e66394caf1d12a4350cc2f454bb0edb6a233afd
LawsonStation <62918031+LawsonStation@users.noreply.github.com>
2024-02-11 04:12:00 -0500

    crkbd keymap updates (WIP)

--- keyboards/crkbd/keymaps/LawsonStation/keymap.c
+++ keyboards/crkbd/keymaps/LawsonStation/keymap.c
@@ -19,12 +19,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
+// Define macros
+#define LCTL_ESC LCTL_T(KC_ESC)	
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,------------------------------------------------------------.                    ,--------------------------------------------------------------.
       KC_TAB       ,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINS,
   //|--------------+--------+--------+--------+--------+--------|                    |-----------------+--------+--------+--------+--------+--------|
-      KTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                  KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      LCTL_ESC     ,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                  KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------------+--------+--------+--------+--------+--------|                    |-----------------+--------+--------+--------+--------+--------|
       KC_LSFT      ,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DEL,
   //|--------------+--------+--------+--------+--------+--------+--------|  |--------+-----------------+--------+--------+--------+--------+--------|
@@ -35,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRV      ,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+       _______      , XXXXXXX,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|--------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KTL_T(KC_ESC), XXXXXXX, KC_END,  KC_PGDN, KC_PGUP, KC_HOME,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      KTL_T(KC_ESC),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
   //|--------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT      , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
