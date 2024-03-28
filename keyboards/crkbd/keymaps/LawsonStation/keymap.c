11c8afb2e4ab07aec9abca583addbdbe8751da0a
LawsonStation <62918031+LawsonStation@users.noreply.github.com>
2024-03-28 01:02:56 -0700

    Keymap updates

--- keyboards/crkbd/keymaps/LawsonStation/keymap.c
+++ keyboards/crkbd/keymaps/LawsonStation/keymap.c
@@ -19,10 +19,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
-// Define macros
-#define LCTL_ESC LCTL_T(KC_ESC)	
+// Define macros.
+#define LCTL_ESC LCTL_T(KC_ESC)	 // Mod-Tap for Left Control and Escape.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // Default layer.
   [0] = LAYOUT_split_3x6_3(
   //,------------------------------------------------------------.                    ,--------------------------------------------------------------.
       KC_TAB       ,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINS,
@@ -31,23 +32,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------------+--------+--------+--------+--------+--------|                    |-----------------+--------+--------+--------+--------+--------|
       KC_LSFT      ,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DEL,
   //|--------------+--------+--------+--------+--------+--------+--------|  |--------+-----------------+--------+--------+--------+--------+--------|
-                                                KC_LALT,   MO(1),  KC_SPC,     KC_ENT,   LT(2, KC_BSPC), KC_LGUI
+                                                KC_LALT,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_BSPC
                                             //`--------------------------'  `-----------------------------------'
 
   ),
-
+  // Navigation Layer.
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------------.                    ,-----------------------------------------------------.
        _______      , XXXXXXX,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|--------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KTL_T(KC_ESC),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
+      LCTL_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
   //|--------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT      , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LALT, _______,  KC_SPC,     KC_ENT,   MO(3), KC_LGUI
                                       //`--------------------------'  `--------------------------'
   ),
-
+  // Number layer.
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
@@ -59,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                           KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
-
+  // Adjust (function) layer.
   [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
