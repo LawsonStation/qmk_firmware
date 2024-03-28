2504a90a8ffbe3034dbee2b26429cd61f14c9810
LawsonStation <62918031+LawsonStation@users.noreply.github.com>
2024-03-28 01:15:40 -0700

    Lily 58 navigation keymap changes

--- keyboards/lily58/keymaps/lawsonstation/keymap.c
+++ keyboards/lily58/keymaps/lawsonstation/keymap.c
@@ -1,5 +1,8 @@
 #include QMK_KEYBOARD_H
 
+// Define macros.
+#define LCTL_ESC LCTL_T(KC_ESC)	 // Mod-Tap for Left Control and Escape.
+
 enum layer_number {
   _QWERTY = 0,
   _RAISE,
@@ -27,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  CTL_T(KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  LCTL_ESC, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LPRN,  KC_RPRN,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,
                         KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_DEL
 ),
@@ -56,11 +59,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |   `  |   1  |   2  |   3  | 4    | Home |                    |   6  |   7  |   8  |   9  |   0  |BackSP|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TAB  |MS_BT3|MSWH_D|MS_UP |MSWH_U |PGUP |                    | Home | PGUP | PGDN | End  |   P  |  -   |
+ * | TAB  |MS_BT3|MSWH_D|MS_UP |MSWH_U |PGUP |                    |      |      |      |      |   P  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL/ESC|MS_BT1| MS_L |MS_DN | MS_R| PGDN  |-------.    ,-------|  Left | Down |  Up  |Right|  ;  | "   |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LSHIFT|MS_BT2|  X   | C    | V    | End  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |LSHIFT|MS_BT2|  X   | C    | V    | End  |-------|    |-------| Home | PGUP | PGDN | End  |   /  |Enter |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP| DEL  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -70,8 +73,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   KC_GRV,  KC_1,  	    KC_2,     KC_3,       KC_4,   	KC_HOME,                     			KC_6,      KC_7,    KC_8, 		KC_9,		KC_0, 		KC_BSPC,
   KC_TAB, 	KC_BTN3,    KC_WH_D,  KC_MS_U, 	  KC_WH_U,  KC_PGUP,                        		KC_HOME,  KC_PGUP,  KC_PGDN,    KC_END, 	KC_P,    	KC_MINS,
-  CTL_T(KC_ESC),  KC_BTN1,    KC_MS_L,  KC_MS_D,    KC_MS_R,  KC_PGDN,                       		KC_LEFT,  KC_DOWN,  KC_UP, 		KC_RGHT, 	KC_SCLN, 	KC_QUOT,
-  KC_LSFT,  KC_BTN2,    KC_X,     KC_C,       KC_V,     KC_END,   	KC_LBRC, 	KC_RBRC,	KC_N,      KC_M,    KC_COMM, 	KC_DOT,  	KC_SLSH,  	KC_ENT,
+  LCTL_ESC,  KC_BTN1,    KC_MS_L,  KC_MS_D,    KC_MS_R,  KC_PGDN,                       		KC_LEFT,  KC_DOWN,  KC_UP, 		KC_RGHT, 	KC_SCLN, 	KC_QUOT,
+  KC_LSFT,  KC_BTN2,    KC_X,     KC_C,       KC_V,     KC_END,   	KC_LBRC, 	KC_RBRC,	KC_HOME,      KC_PGUP,    KC_PGDN, 	KC_END,  	KC_SLSH,  	KC_ENT,
 								_______,      _______,    _______,  _______, 	_______,    _______,   	_______,    _______
 ),
 /* ADJUST
