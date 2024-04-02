e7d66b8e886ab48485664c8eca0049a1d804e41c
LawsonStation <iamkevinho@gmail.com>
2024-04-02 01:01:26 -0700

    Add home row mods to number layer (left hand)

--- keyboards/crkbd/keymaps/LawsonStation/keymap.c
+++ keyboards/crkbd/keymaps/LawsonStation/keymap.c
@@ -34,6 +34,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LALT_L LALT_T(KC_L)
 #define LCTL_M LCTL_T(KC_M)
 
+#define LCTL_3 LCTL_T(KC_3)
+#define LALT_4 LALT_T(KC_4)
+#define LGUI_5 LGUI_T(KC_5)
+#define LSFT_6 LSFT_T(KC_6)
+
 #define LCTL_F3 LCTL_T(KC_F3)
 #define LALT_F4 LALT_T(KC_F4)
 #define LGUI_F5 LGUI_T(KC_F5)
@@ -74,9 +79,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, KC_BSLS,    KC_7,    KC_8,    KC_9, KC_LBRC,                      KC_RBRC, KC_PPLS, KC_PMNS, KC_PAST, KC_PSLS, KC_MINS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     LCTL_ESC,  KC_EQL,    KC_4,    KC_5,    KC_6, KC_LPRN,                      KC_RPRN, KC_LSFT, KC_LGUI, KC_LALT,  KC_DLR, KC_PERC,
+     LCTL_ESC,  KC_EQL,  LALT_4,  LGUI_5,  LSFT_6, KC_LPRN,                      KC_RPRN, KC_LSFT, KC_LGUI, KC_LALT,  KC_DLR, KC_PERC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_MINS,    KC_1,    KC_2,    KC_3,  KC_GRV,                       KC_EQL, KC_LCTL, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
+      KC_LSFT, KC_MINS,    KC_1,    KC_2,  LCTL_3,  KC_GRV,                       KC_EQL, KC_LCTL, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                            KC_DOT,    KC_0,  KC_SPC,     KC_ENT, XXXXXXX, KC_BSPC
                                       //`--------------------------'  `--------------------------'
