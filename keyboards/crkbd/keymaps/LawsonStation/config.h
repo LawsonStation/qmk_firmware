91415d912f1da5b75fe5cda6c9f49789b4179d51
LawsonStation <iamkevinho@gmail.com>
2024-04-14 16:38:50 -0700

    Change crkbd TAPPING_TERM from 150 to 200

--- keyboards/crkbd/keymaps/LawsonStation/config.h
+++ keyboards/crkbd/keymaps/LawsonStation/config.h
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 150
+#define TAPPING_TERM 200
 #define QUICK_TAP_TERM 0  //prevent quick repeated keys from being read as a hold
 // #define PERMISSIVE_HOLD  //dual-role key sends hold action when another key is pressed within tapping term
 
