4e43e7637cb35000d0c116d05df3e7128eaecf1c
kevin <iamkevinho@gmail.com>
2024-09-08 22:45:00 -0700

    Add L_QWERTY2 to layer_state_reader.c

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #define L_BASE 0
+#define L_QWERTY2 (1 << 1)
 #define L_RAISE (1 << 3)
 #define L_LOWER (1 << 4)
 #define L_ADJUST (1 << 5)
@@ -14,6 +15,9 @@ const char *read_layer_state(void) {
   {
   case L_BASE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    break;
+  case L_QWERTY2:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Qwerty2");
     break;
   case L_RAISE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
