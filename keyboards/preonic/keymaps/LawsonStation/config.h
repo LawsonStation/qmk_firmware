1ae4cffa02d27898f3f223331c6e4c10d627de20
LawsonStation <iamkevinho@gmail.com>
2024-09-09 01:05:02 -0700

    Fix Preonic config.h comments

--- keyboards/preonic/keymaps/LawsonStation/config.h
+++ keyboards/preonic/keymaps/LawsonStation/config.h
@@ -20,9 +20,11 @@
 // #    define STARTUP_SONG SONG(PREONIC_SOUND)
 // #define STARTUP_SONG SONG(NO_SOUND)
 
-// #    define DEFAULT_LAYER_SONGS \
+/*
+#    define DEFAULT_LAYER_SONGS \
         { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
-// #endif
+#endif
+*/
 
 // #define MUSIC_MASK (keycode != KC_NO)
 
