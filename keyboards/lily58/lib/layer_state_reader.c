1c8e76f20000c49fdc1c2f68a53fe4c1299c0654
kevin <iamkevinho@gmail.com>
2024-09-08 20:44:03 -0700

    Revert layer_state_reader.c to default

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -1,33 +1,33 @@
 #include "quantum.h"
 #include <stdio.h>
 
+#define L_BASE 0
+#define L_RAISE (1 << 1)
+#define L_LOWER (1 << 2)
+#define L_ADJUST (1 << 3)
+#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+
 char layer_state_str[24];
 
-enum layer_names {
-    _QWERTY,
-    _QWERTY2,
-    _COLEMAK,
-    _LOWER,
-    _RAISE,
-    _ADJUST
-};
-
 const char *read_layer_state(void) {
-    if (layer_state_cmp(layer_state, _QWERTY)) {
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY");
-    } else if (layer_state_cmp(layer_state, _QWERTY2)) {
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY2");
-    } else if (layer_state_cmp(layer_state, _COLEMAK)) {
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: COLEMAK");
-    } else if (layer_state_cmp(layer_state, _LOWER)) {
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: LOWER");
-    } else if (layer_state_cmp(layer_state, _RAISE)) {
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: RAISE");
-    } else if (layer_state_cmp(layer_state, _ADJUST)) {
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: ADJUST");
-    } else {
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
-    }
+  switch (layer_state)
+  {
+  case L_BASE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    break;
+  case L_RAISE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+    break;
+  case L_LOWER:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+    break;
+  case L_ADJUST:
+  case L_ADJUST_TRI:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+    break;
+  default:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
+  }
 
-    return layer_state_str;
+  return layer_state_str;
 }
