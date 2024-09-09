580dc75a3c9dcc62ece20ab5ff9f5fed71116176
kevin <iamkevinho@gmail.com>
2024-09-08 18:44:25 -0700

    Updates Lily58 keymap `layer_state_reader.c` for additional layers v3

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -17,7 +17,7 @@ const char *read_layer_state(void) {
     } else if (layer_state_cmp(layer_state, _ADJUST)) {
         snprintf(layer_state_str, sizeof(layer_state_str), "Layer: ADJUST");
     } else {
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%lu", layer_state);
+        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
     }
 
     return layer_state_str;
