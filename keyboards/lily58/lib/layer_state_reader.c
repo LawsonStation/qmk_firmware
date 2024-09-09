a5bb6724380151d9c388dce572125a8d0818c67d
kevin <iamkevinho@gmail.com>
2024-09-08 21:43:50 -0700

    Test new layer_state_reader.c functionality (other base layers)

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -1,32 +1,31 @@
 #include "quantum.h"
 #include <stdio.h>
 
-#define L_BASE 0
-#define L_RAISE (1 << 1)
-#define L_LOWER (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
-
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
-  switch (layer_state)
+  switch (get_highest_layer(layer_state))
   {
-  case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+  case _QWERTY:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY");
     break;
-  case L_RAISE:
+  case _QWERTY2:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY2");
+    break;
+  case _COLEMAK:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Colemak");
+    break;
+  case _RAISE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
     break;
-  case L_LOWER:
+  case _LOWER:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
     break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
+  case _ADJUST:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
     break;
   default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", get_highest_layer(layer_state));
   }
 
   return layer_state_str;
